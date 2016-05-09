9e24e38128e72cb0ee26201394c8b8f72e3171b4
MrCrayfish <mrcrayfish@hotmail.com>
2016-05-09 16:03:56 +0930

    Optimised loading levels.

--- src/main/java/com/mrcrayfish/device/object/Game.java
+++ src/main/java/com/mrcrayfish/device/object/Game.java
@@ -46,26 +46,17 @@ public class Game extends Component
 		{
 			InputStream input = getClass().getClassLoader().getResourceAsStream("assets/cdm/map.png");
 			BufferedImage image = ImageIO.read(input);
-			int width = image.getWidth();
-			int height = image.getHeight();
-			for(int y = 0; y < height; y++)
+			int w = image.getWidth();
+			int h = image.getHeight();
+			int[] tilesInt = new int[w * h];
+			image.getRGB(0, 0, w, h, tilesInt, 0, w);
+			
+			for(int i = 0; i < tilesInt.length; i++)
 			{
-				for(int x = 0; x < width; x++)
-				{
-					int col = image.getRGB(x, y);
-					switch(col)
-					{
-						case -12566464:
-							backgroundTiles[x + y * mapWidth] = Tile.log;
-							break;
-						case -16744690:
-							backgroundTiles[x + y * mapWidth] = Tile.grass;
-							break;
-						default:
-							backgroundTiles[x + y * mapWidth] = Tile.water;
-							break;
-					}
-				}
+				int col = tilesInt[i];
+				if(col == 0xFF404040) backgroundTiles[i] = Tile.log;
+				if(col == 0xFF007F0E) backgroundTiles[i] = Tile.grass;
+				if(col == 0xFF0094FF) backgroundTiles[i] = Tile.water;
 			}
 		}
 		catch (IOException e)
