1867c72f683e54c56faab69d1e23f61dde8a33cb
MrCrayfish <mrcrayfish@hotmail.com>
2016-05-09 13:18:03 +0930

    Grass tile now renders edges if adjacent tile is water

--- src/main/java/com/mrcrayfish/device/object/tiles/TileGrass.java
+++ src/main/java/com/mrcrayfish/device/object/tiles/TileGrass.java
@@ -9,6 +9,32 @@ public class TileGrass extends Tile
 	{
 		super(x, y);
 	}
+
+	@Override
+	public void renderForeground(Game game, int x, int y)
+	{
+		super.renderForeground(game, x, y);
+		
+		if(game.getTile(x, y - 1) == Tile.water)
+		{
+			GuiHelper.drawModalRectWithUV(game.xPosition + x * Tile.SIZE, game.yPosition + y * Tile.SIZE - 1, 16, 16, 8, 1, 16, 2);
+		}
+		
+		if(game.getTile(x, y + 1) == Tile.water)
+		{
+			GuiHelper.drawModalRectWithUV(game.xPosition + x * Tile.SIZE, game.yPosition + y * Tile.SIZE + 8, 16, 18, 8, 1, 16, 2);
+		}
+		
+		if(game.getTile(x - 1, y) == Tile.water)
+		{
+			GuiHelper.drawModalRectWithUV(game.xPosition + x * Tile.SIZE - 1, game.yPosition + y * Tile.SIZE, 0, 16, 1, 8, 2, 16);
+		}
+		
+		if(game.getTile(x + 1, y) == Tile.water)
+		{
+			GuiHelper.drawModalRectWithUV(game.xPosition + x * Tile.SIZE + 8, game.yPosition + y * Tile.SIZE, 2, 16, 1, 8, 2, 16);
+		}
+	}
 	
 	@Override
 	public void render(Game game, int x, int y)
