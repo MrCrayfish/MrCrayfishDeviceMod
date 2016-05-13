afe33128c2f750eb5996a50de647a86e24d452b9
MrCrayfish <mrcrayfish@hotmail.com>
2016-05-13 10:39:23 +0930

    Added Categories for Tiles
    Eventually be used for the level editior

--- src/main/java/com/mrcrayfish/device/object/tiles/Tile.java
+++ src/main/java/com/mrcrayfish/device/object/tiles/Tile.java
@@ -6,25 +6,33 @@ import com.mrcrayfish.device.util.GuiHelper;
 public class Tile
 {
 	// Blocks
-	public static final Tile grass = new TileGrass(1, 0);
-	public static final Tile water = new Tile(2, 0);
-	public static final Tile log = new Tile(3, 0);
-	public static final Tile farm_land = new Tile(5, 0);
+	public static final Tile grass = new TileGrass(1, 0).setCategory(Category.BLOCKS);
+	public static final Tile water = new Tile(2, 0).setCategory(Category.BLOCKS);
+	public static final Tile log = new Tile(3, 0).setCategory(Category.BLOCKS);
+	public static final Tile farm_land = new Tile(5, 0).setCategory(Category.BLOCKS);
 	
 	// Details
-	public static final Tile red_flower = new Tile(0, 2);
-	public static final Tile lily_pad = new Tile(1, 2);
-	public static final Tile wheat = new TileWheat(2, 2);
+	public static final Tile red_flower = new Tile(0, 2).setCategory(Category.DECORATION);
+	public static final Tile lily_pad = new Tile(1, 2).setCategory(Category.DECORATION);
+	public static final Tile wheat = new TileWheat(2, 2).setCategory(Category.DECORATION);
 
 	public static final int SIZE = 8;
 	
 	protected final int x;
 	protected final int y;
 	
+	private Category category;
+	
 	public Tile(int x, int y)
 	{
 		this.x = x;
 		this.y = y;
+	}
+	
+	public Tile setCategory(Category category)
+	{
+		this.category = category;
+		return this;
 	}
 	
 	public void render(Game game, int x, int y)
@@ -42,5 +50,15 @@ public class Tile
 	public boolean isSlow()
 	{
 		return false;
+	}
+
+	public Category getCategory()
+	{
+		return category;
+	}
+	
+	public static enum Category 
+	{
+		BLOCKS, DECORATION, GAME;
 	}
 }
