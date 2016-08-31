0d9a2c64a140d88d40d676df5e9611bc1bc7f495
MrCrayfish <mrcrayfish@hotmail.com>
2016-09-01 02:23:52 +0930

    Fixed lighting bug when rendering block in GUI

--- src/main/java/com/mrcrayfish/device/api/utils/RenderUtil.java
+++ src/main/java/com/mrcrayfish/device/api/utils/RenderUtil.java
@@ -2,6 +2,7 @@ package com.mrcrayfish.device.api.utils;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.item.ItemStack;
 
 public class RenderUtil
@@ -9,8 +10,10 @@ public class RenderUtil
 	public static void renderItem(int x, int y, ItemStack stack, boolean overlay)
 	{
 		GlStateManager.disableDepth();
-		Minecraft.getMinecraft().getRenderItem().renderItemIntoGUI(stack, x, y);
+		RenderHelper.enableGUIStandardItemLighting();
+		Minecraft.getMinecraft().getRenderItem().renderItemAndEffectIntoGUI(stack, x, y);
 		if(overlay) Minecraft.getMinecraft().getRenderItem().renderItemOverlays(Minecraft.getMinecraft().fontRendererObj, stack, x, y);
+		GlStateManager.enableAlpha();
 		GlStateManager.disableLighting();
 		GlStateManager.enableDepth();
 	}
