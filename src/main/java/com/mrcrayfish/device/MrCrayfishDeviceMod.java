ae25f07c2264c1a179217975b584e5b1a956f4b4
MrCrayfish <mrcrayfish@hotmail.com>
2017-09-07 17:09:06 +0930

    Merged Task handling into one class

--- src/main/java/com/mrcrayfish/device/MrCrayfishDeviceMod.java
+++ src/main/java/com/mrcrayfish/device/MrCrayfishDeviceMod.java
@@ -1,10 +1,7 @@
 package com.mrcrayfish.device;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-
 import com.mrcrayfish.device.api.ApplicationManager;
-import com.mrcrayfish.device.api.task.TaskProxy;
+import com.mrcrayfish.device.api.task.TaskManager;
 import com.mrcrayfish.device.core.Laptop;
 import com.mrcrayfish.device.event.BankEvents;
 import com.mrcrayfish.device.event.EmailEvents;
@@ -13,30 +10,22 @@ import com.mrcrayfish.device.init.DeviceBlocks;
 import com.mrcrayfish.device.init.DeviceCrafting;
 import com.mrcrayfish.device.init.DeviceTileEntites;
 import com.mrcrayfish.device.network.PacketHandler;
-import com.mrcrayfish.device.network.task.TaskManager;
 import com.mrcrayfish.device.programs.ApplicationBoatRacers;
 import com.mrcrayfish.device.programs.ApplicationExample;
 import com.mrcrayfish.device.programs.ApplicationNoteStash;
 import com.mrcrayfish.device.programs.ApplicationPixelPainter;
-import com.mrcrayfish.device.programs.ApplicationTest;
 import com.mrcrayfish.device.programs.auction.ApplicationMineBay;
 import com.mrcrayfish.device.programs.auction.task.TaskAddAuction;
 import com.mrcrayfish.device.programs.auction.task.TaskBuyItem;
 import com.mrcrayfish.device.programs.auction.task.TaskGetAuctions;
 import com.mrcrayfish.device.programs.email.ApplicationEmail;
-import com.mrcrayfish.device.programs.email.task.TaskCheckEmailAccount;
-import com.mrcrayfish.device.programs.email.task.TaskDeleteEmail;
-import com.mrcrayfish.device.programs.email.task.TaskRegisterEmailAccount;
-import com.mrcrayfish.device.programs.email.task.TaskSendEmail;
-import com.mrcrayfish.device.programs.email.task.TaskUpdateInbox;
-import com.mrcrayfish.device.programs.email.task.TaskViewEmail;
+import com.mrcrayfish.device.programs.email.task.*;
 import com.mrcrayfish.device.programs.system.ApplicationBank;
 import com.mrcrayfish.device.programs.system.task.TaskAdd;
 import com.mrcrayfish.device.programs.system.task.TaskGetBalance;
 import com.mrcrayfish.device.programs.system.task.TaskPay;
 import com.mrcrayfish.device.programs.system.task.TaskRemove;
 import com.mrcrayfish.device.proxy.IProxyInterface;
-
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.launchwrapper.Launch;
 import net.minecraft.util.ResourceLocation;
@@ -49,6 +38,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
+import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.WORKING_MC_VERSION)
 public class MrCrayfishDeviceMod 
@@ -61,7 +51,9 @@ public class MrCrayfishDeviceMod
 	
 	public static CreativeTabs tabDevice = new DeviceTab("cdmTabDevice");
 
-	public static final boolean DEVELOPER_MODE = false;
+	private static Logger logger;
+
+	public static final boolean DEVELOPER_MODE = true;
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) throws LaunchException {
@@ -71,8 +63,8 @@ public class MrCrayfishDeviceMod
 			throw new LaunchException();
 		}
 
-		setupTaskProxy();
-		
+		logger = event.getModLog();
+
 		/** Block Registering */
 		DeviceBlocks.init();
 		DeviceBlocks.register();
@@ -110,10 +102,10 @@ public class MrCrayfishDeviceMod
 		ApplicationManager.registerApplication(new ApplicationBank());
 
 		// Tasks (Both)
-		TaskProxy.registerTask(TaskGetBalance.class);
-		TaskProxy.registerTask(TaskPay.class);
-		TaskProxy.registerTask(TaskAdd.class);
-		TaskProxy.registerTask(TaskRemove.class);
+		TaskManager.registerTask(TaskGetBalance.class);
+		TaskManager.registerTask(TaskPay.class);
+		TaskManager.registerTask(TaskAdd.class);
+		TaskManager.registerTask(TaskRemove.class);
 
 		if(!DEVELOPER_MODE)
 		{
@@ -134,15 +126,15 @@ public class MrCrayfishDeviceMod
 			Laptop.addWallpaper(new ResourceLocation("cdm:textures/gui/laptop_wallpaper_6.png"));
 
 			// Tasks (Normal)
-			TaskProxy.registerTask(TaskUpdateInbox.class);
-			TaskProxy.registerTask(TaskSendEmail.class);
-			TaskProxy.registerTask(TaskCheckEmailAccount.class);
-			TaskProxy.registerTask(TaskRegisterEmailAccount.class);
-			TaskProxy.registerTask(TaskDeleteEmail.class);
-			TaskProxy.registerTask(TaskViewEmail.class);
-			TaskProxy.registerTask(TaskAddAuction.class);
-			TaskProxy.registerTask(TaskGetAuctions.class);
-			TaskProxy.registerTask(TaskBuyItem.class);
+			TaskManager.registerTask(TaskUpdateInbox.class);
+			TaskManager.registerTask(TaskSendEmail.class);
+			TaskManager.registerTask(TaskCheckEmailAccount.class);
+			TaskManager.registerTask(TaskRegisterEmailAccount.class);
+			TaskManager.registerTask(TaskDeleteEmail.class);
+			TaskManager.registerTask(TaskViewEmail.class);
+			TaskManager.registerTask(TaskAddAuction.class);
+			TaskManager.registerTask(TaskGetAuctions.class);
+			TaskManager.registerTask(TaskBuyItem.class);
 		}
 		else
 		{
@@ -155,39 +147,9 @@ public class MrCrayfishDeviceMod
 		
 		proxy.postInit();
 	}
-	
-	private void setupTaskProxy()
+
+	public static Logger getLogger()
 	{
-		try
-		{
-			Constructor<TaskManager> constructor = TaskManager.class.getDeclaredConstructor();
-			constructor.setAccessible(true);
-			TaskManager manager = constructor.newInstance();
-			TaskProxy.setInstance(manager);
-		}
-		catch (NoSuchMethodException e)
-		{
-			e.printStackTrace();
-		}
-		catch (SecurityException e)
-		{
-			e.printStackTrace();
-		}
-		catch (InstantiationException e)
-		{
-			e.printStackTrace();
-		}
-		catch (IllegalAccessException e)
-		{
-			e.printStackTrace();
-		}
-		catch (IllegalArgumentException e)
-		{
-			e.printStackTrace();
-		}
-		catch (InvocationTargetException e)
-		{
-			e.printStackTrace();
-		}
+		return logger;
 	}
 }
---
+++ src/main/java/com/mrcrayfish/device/api/task/TaskManager.java
@@ -0,0 +1,72 @@
+package com.mrcrayfish.device.api.task;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.mrcrayfish.device.api.task.Task;
+import com.mrcrayfish.device.network.PacketHandler;
+import com.mrcrayfish.device.network.task.MessageRequest;
+
+public final class TaskManager
+{
+	private static TaskManager instance = null;
+
+	private Map<String, Task> registeredRequests = new HashMap<String, Task>();
+	private Map<Integer, Task> requests = new HashMap<Integer, Task>();
+	private int currentId = 0;
+
+	private TaskManager() {}
+
+	private static TaskManager get()
+	{
+		if(instance == null)
+		{
+			instance = new TaskManager();
+		}
+		return instance;
+	}
+	
+	public static void registerTask(Class<? extends Task> clazz)
+	{
+		try 
+		{
+			Constructor<? extends Task> constructor = clazz.getDeclaredConstructor();
+			constructor.setAccessible(true);
+			Task task = (Task) constructor.newInstance();
+			System.out.println("Registering task '" + task.getName() + "'");
+			get().registeredRequests.put(task.getName(), task);
+		} 
+		catch (InstantiationException e) 
+		{	
+			System.err.println("- Missing constructor '" + clazz.getSimpleName() + "()'");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void sendTask(Task task)
+	{
+		TaskManager manager = get();
+		if(!manager.registeredRequests.containsKey(task.getName())) {
+			throw new RuntimeException("Unregistered Task: " + task.getClass().getName() + ". Use TaskManager#requestRequest to register your task.");
+		}
+		
+		int requestId = manager.currentId++;
+		manager.requests.put(requestId, task);
+		PacketHandler.INSTANCE.sendToServer(new MessageRequest(requestId, task));
+	}
+	
+	public static Task getTask(String name)
+	{
+		return get().registeredRequests.get(name);
+	}
+
+	public static Task getTaskAndRemove(int id)
+	{
+		return get().requests.remove(id);
+	}
+}
