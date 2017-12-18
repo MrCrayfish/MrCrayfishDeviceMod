1b90d9fd9f98713b9b40345dba0c00f74a02e166
MrCrayfish <mrcrayfish@hotmail.com>
2017-12-18 12:12:31 +1030

    Added CPU

--- src/main/java/com/mrcrayfish/device/init/DeviceItems.java
+++ src/main/java/com/mrcrayfish/device/init/DeviceItems.java
@@ -1,5 +1,6 @@
 package com.mrcrayfish.device.init;
 
+import com.mrcrayfish.device.item.ItemComponent;
 import com.mrcrayfish.device.item.ItemEthernetCable;
 import com.mrcrayfish.device.item.ItemFlashDrive;
 import net.minecraft.item.Item;
@@ -12,16 +13,20 @@ public class DeviceItems
     public static final Item FLASH_DRIVE;
     public static final Item ETHERNET_CABLE;
 
+    public static final Item COMPONENT_CPU;
+
     static
     {
         FLASH_DRIVE = new ItemFlashDrive();
         ETHERNET_CABLE = new ItemEthernetCable();
+        COMPONENT_CPU = new ItemComponent("cpu");
     }
 
     public static void register()
     {
         register(FLASH_DRIVE);
         register(ETHERNET_CABLE);
+        register(COMPONENT_CPU);
     }
 
     private static void register(Item item)
