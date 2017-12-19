d8a8df0fbce8454f63ca53b9f54f8538063d4125
MrCrayfish <mrcrayfish@hotmail.com>
2017-12-20 00:46:14 +1030

    Added tool tips to items

--- src/main/java/com/mrcrayfish/device/item/ItemDevice.java
+++ src/main/java/com/mrcrayfish/device/item/ItemDevice.java
@@ -2,15 +2,19 @@ package com.mrcrayfish.device.item;
 
 import com.mrcrayfish.device.Reference;
 import net.minecraft.block.Block;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import org.apache.commons.lang3.text.WordUtils;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  * Author: MrCrayfish
@@ -29,6 +33,15 @@ public class ItemDevice extends ItemBlock implements SubItems
     public boolean getShareTag()
     {
         return false;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+        EnumDyeColor color = EnumDyeColor.byMetadata(stack.getMetadata());
+        String colorName = color.getName().replace("_", " ");
+        colorName = WordUtils.capitalize(colorName);
+        tooltip.add("Color: " + TextFormatting.BOLD.toString() + getFromColor(color).toString() + colorName);
     }
 
     @Override
@@ -52,5 +65,28 @@ public class ItemDevice extends ItemBlock implements SubItems
             modelLocations.add(new ResourceLocation(Reference.MOD_ID, getUnlocalizedName().substring(5) + "/" + color.getName()));
         }
         return modelLocations;
+    }
+
+    private static TextFormatting getFromColor(EnumDyeColor color)
+    {
+        switch(color)
+        {
+            case ORANGE: return TextFormatting.GOLD;
+            case MAGENTA: return TextFormatting.LIGHT_PURPLE;
+            case LIGHT_BLUE: return TextFormatting.BLUE;
+            case YELLOW: return TextFormatting.YELLOW;
+            case LIME: return TextFormatting.GREEN;
+            case PINK: return TextFormatting.LIGHT_PURPLE;
+            case GRAY: return TextFormatting.DARK_GRAY;
+            case SILVER: return TextFormatting.GRAY;
+            case CYAN: return TextFormatting.DARK_AQUA;
+            case PURPLE: return TextFormatting.DARK_PURPLE;
+            case BLUE: return TextFormatting.DARK_BLUE;
+            case BROWN: return TextFormatting.GOLD;
+            case GREEN: return TextFormatting.DARK_GREEN;
+            case RED: return TextFormatting.DARK_RED;
+            case BLACK: return TextFormatting.BLACK;
+            default: return TextFormatting.WHITE;
+        }
     }
 }
