d8a5ffe9b9681365713c07c63165395600318c43
MrCrayfish <mrcrayfish@hotmail.com>
2017-12-21 21:00:27 +1030

    Fixed chair color not saving

--- src/main/java/com/mrcrayfish/device/tileentity/TileEntityOfficeChair.java
+++ src/main/java/com/mrcrayfish/device/tileentity/TileEntityOfficeChair.java
@@ -2,12 +2,14 @@ package com.mrcrayfish.device.tileentity;
 
 import com.mrcrayfish.device.util.Colorable;
 import net.minecraft.item.EnumDyeColor;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.util.Constants;
 
 /**
  * Author: MrCrayfish
  */
-public class TileEntityOfficeChair extends TileEntity implements Colorable
+public class TileEntityOfficeChair extends TileEntitySync implements Colorable
 {
     private EnumDyeColor color = EnumDyeColor.RED;
 
@@ -21,5 +23,31 @@ public class TileEntityOfficeChair extends TileEntity implements Colorable
     public void setColor(EnumDyeColor color)
     {
         this.color = color;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound)
+    {
+        super.readFromNBT(compound);
+        if(compound.hasKey("color", Constants.NBT.TAG_BYTE))
+        {
+            color = EnumDyeColor.byMetadata(compound.getByte("color"));
+        }
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound)
+    {
+        super.writeToNBT(compound);
+        compound.setByte("color", (byte) color.getMetadata());
+        return compound;
+    }
+
+    @Override
+    public NBTTagCompound writeSyncTag()
+    {
+        NBTTagCompound tag = new NBTTagCompound();
+        tag.setByte("color", (byte) color.getMetadata());
+        return tag;
     }
 }
