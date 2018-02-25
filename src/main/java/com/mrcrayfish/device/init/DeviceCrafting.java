b896f7f023285a990b53b812231dda2ff5abc36f
MrCrayfish <mrcrayfish@hotmail.com>
2018-02-25 15:35:31 +1030

    Increased plastic smelting amount

--- src/main/java/com/mrcrayfish/device/init/DeviceCrafting.java
+++ src/main/java/com/mrcrayfish/device/init/DeviceCrafting.java
@@ -13,6 +13,6 @@ public class DeviceCrafting
 		RegistrationHandler.Recipes.add(new RecipeCutPaper());
 		RegistrationHandler.Recipes.add(new RecipeMotherboard());
 		RegistrationHandler.Recipes.add(new RecipeLaptop());
-		GameRegistry.addSmelting(DeviceItems.PLASTIC_UNREFINED, new ItemStack(DeviceItems.PLASTIC, 2), 0.2F);
+		GameRegistry.addSmelting(DeviceItems.PLASTIC_UNREFINED, new ItemStack(DeviceItems.PLASTIC, 4), 0.2F);
 	}
 }
