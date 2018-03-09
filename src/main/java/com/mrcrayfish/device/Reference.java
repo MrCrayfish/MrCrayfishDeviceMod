937a86af0f9230ed954e96aaeef745f53603431a
MrCrayfish <mrcrayfish@hotmail.com>
2018-03-09 20:05:19 +1030

    Updated version

--- src/main/java/com/mrcrayfish/device/Reference.java
+++ src/main/java/com/mrcrayfish/device/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MOD_ID = "cdm";
 	public static final String NAME = "MrCrayfish's Device Mod";
-	public static final String VERSION = "0.3.0";
+	public static final String VERSION = "0.3.1";
 	public static final String CLIENT_PROXY_CLASS = "com.mrcrayfish.device.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.mrcrayfish.device.proxy.CommonProxy";
 	public static final String WORKING_MC_VERSION = "[1.12.2]";
