2717935a51bfd4f05ee8b96393acccf5821b6cb9
Ocelot5836 <FinntheRaider@gmail.com>
2018-04-11 17:19:00 -0700

    added docs and another method

--- src/main/java/com/mrcrayfish/device/util/GLHelper.java
+++ src/main/java/com/mrcrayfish/device/util/GLHelper.java
@@ -1,33 +1,66 @@
 package com.mrcrayfish.device.util;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.ScaledResolution;
+import java.awt.Color;
+import java.nio.FloatBuffer;
+
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 
-import java.awt.*;
-import java.nio.FloatBuffer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.GlStateManager;
 
 /**
  * Author: MrCrayfish
  */
-public class GLHelper
-{
-    public static void scissor(int x, int y, int width, int height)
-    {
-        Minecraft mc = Minecraft.getMinecraft();
-        ScaledResolution resolution = new ScaledResolution(mc);
-        int scale = resolution.getScaleFactor();
-        GL11.glScissor(x * scale, mc.displayHeight - y * scale - height * scale, width * scale, height *scale);
-    }
+public class GLHelper {
 
-    public static Color getPixel(int x, int y)
-    {
-        Minecraft mc = Minecraft.getMinecraft();
-        ScaledResolution resolution = new ScaledResolution(mc);
-        int scale = resolution.getScaleFactor();
-        FloatBuffer buffer = BufferUtils.createFloatBuffer(3);
-        GL11.glReadPixels(x * scale, mc.displayHeight - y * scale - scale, 1, 1, GL11.GL_RGB, GL11.GL_FLOAT, buffer);
-        return new Color(buffer.get(0), buffer.get(1), buffer.get(2));
-    }
+	public static void scissor(int x, int y, int width, int height)
+	{
+		Minecraft mc = Minecraft.getMinecraft();
+		ScaledResolution resolution = new ScaledResolution(mc);
+		int scale = resolution.getScaleFactor();
+		GL11.glScissor(x * scale, mc.displayHeight - y * scale - height * scale, width * scale, height * scale);
+	}
+
+	/**
+	 * Gets the color of the pixel at the specified point
+	 * 
+	 * @param x
+	 *            The x of the pixel
+	 * @param y
+	 *            The y of the pixel
+	 * @return The color of the pixel at that point
+	 */
+	public static Color getPixel(int x, int y)
+	{
+		Minecraft mc = Minecraft.getMinecraft();
+		ScaledResolution resolution = new ScaledResolution(mc);
+		int scale = resolution.getScaleFactor();
+		FloatBuffer buffer = BufferUtils.createFloatBuffer(3);
+		GL11.glReadPixels(x * scale, mc.displayHeight - y * scale - scale, 1, 1, GL11.GL_RGB, GL11.GL_FLOAT, buffer);
+		return new Color(buffer.get(0), buffer.get(1), buffer.get(2));
+	}
+
+	/**
+	 * Sets the currently bound color to the specified color
+	 * 
+	 * @param color
+	 *            The new color to use
+	 */
+	public static void color(Color color)
+	{
+		color(color.getRGB());
+	}
+
+	/**
+	 * Sets the currently bound color to the specified color
+	 * 
+	 * @param color
+	 *            The new color to use
+	 */
+	public static void color(int color)
+	{
+		GlStateManager.color((float) ((color >> 16) & 0xFF) / 255, (float) ((color >> 8) & 0xFF) / 255, (float) ((color >> 0) & 0xFF) / 255, (float) ((color >> 24) & 0xFF) / 255);
+	}
 }
