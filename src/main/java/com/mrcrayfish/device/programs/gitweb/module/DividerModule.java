f55a30f5533a7da4820a6f768b4bd71129c99aab
MrCrayfish <mrcrayfish@hotmail.com>
2018-04-20 16:59:10 +0930

    Added ability to set divder color

--- src/main/java/com/mrcrayfish/device/programs/gitweb/module/DividerModule.java
+++ src/main/java/com/mrcrayfish/device/programs/gitweb/module/DividerModule.java
@@ -2,6 +2,8 @@ package com.mrcrayfish.device.programs.gitweb.module;
 
 import com.mrcrayfish.device.api.app.Layout;
 import com.mrcrayfish.device.programs.gitweb.component.GitWebFrame;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 
 import java.util.Map;
 
@@ -23,5 +25,15 @@ public class DividerModule extends Module
     }
 
     @Override
-    public void generate(GitWebFrame frame, Layout layout, int width, Map<String, String> data) {}
+    public void generate(GitWebFrame frame, Layout layout, int width, Map<String, String> data)
+    {
+        if(data.containsKey("color"))
+        {
+            int color = Integer.parseInt(data.get("color"));
+            layout.setBackground((gui, mc, x, y, width1, height, mouseX, mouseY, windowActive) ->
+            {
+                Gui.drawRect(x, y, x + width1, y + height, color);
+            });
+        }
+    }
 }
