4c4e4a8ebce4ff2635c2dd24c75e05a8b7a3771d
Ocelot5836 <FinntheRaider@gmail.com>
2018-08-22 14:28:14 -0700

    updated Release Listener

--- src/main/java/com/mrcrayfish/device/api/app/listener/ReleaseListener.java
+++ src/main/java/com/mrcrayfish/device/api/app/listener/ReleaseListener.java
@@ -13,8 +13,7 @@ public interface ReleaseListener
 	/**
 	 * Called when a click on a component is released
 	 * 
-	 * @param c the component that was clicked
 	 * @param mouseButton the mouse button used to click
 	 */
-	void onRelease(Component c, int mouseButton);
+	void onRelease(int mouseX, int mouseY, int mouseButton);
 }
