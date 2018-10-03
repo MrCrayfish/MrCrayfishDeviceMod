2dfa29a846c6b1b8c06a86d196c0da1a39950692
Ocelot5836 <FinntheRaider@gmail.com>
2018-10-02 18:13:46 -0700

    Revert "you can no longer resize maximized windows"
    This reverts commit 85d9da51705f373b3f4bd5020de72caad29b21ff.

--- src/main/java/com/mrcrayfish/device/core/Laptop.java
+++ src/main/java/com/mrcrayfish/device/core/Laptop.java
@@ -328,8 +328,8 @@ public class Laptop extends GuiScreen implements System
 
 					window.handleMouseClick(this, posX, posY, mouseX, mouseY, mouseButton);
 
-					Window stretchingWindow = dialogWindow == null ? window : dialogWindow;
-					if (this.isMouseWithinWindow(mouseX, mouseY, stretchingWindow) && stretchingWindow.isDecorated() && !stretchingWindow.isMaximized())
+					Window stretchingWindow = this.isMouseWithinWindow(mouseX, mouseY, window) ? window : dialogWindow;
+					if (stretchingWindow != null)
 					{
 						boolean left = mouseX < posX + stretchingWindow.getOffsetX() + 1;
 						boolean right = mouseX > posX + stretchingWindow.getOffsetX() + stretchingWindow.getWidth() - 2;
@@ -349,14 +349,7 @@ public class Laptop extends GuiScreen implements System
 
 					if (this.isMouseWithinWindowBar(mouseX, mouseY, dialogWindow) && dialogWindow.isDecorated())
 					{
-						if (dialogWindow.isResizable() && dialogWindow.isDecorated() && java.lang.System.currentTimeMillis() - this.lastClick <= 200)
-						{
-							dialogWindow.setMaximized(!dialogWindow.isMaximized());
-						} else
-						{
-							this.lastClick = java.lang.System.currentTimeMillis();
-							this.dragging = true;
-						}
+						this.dragging = true;
 						return;
 					}
 
